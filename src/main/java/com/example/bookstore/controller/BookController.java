40ec3a2ab3a0379e611d2e5c6c78cf874639397a
marinakusniruk <Kushnirukmv@gmail.com>
2023-09-15 12:55:50 +0200

    Book repositoryJPA Hometask completed

--- src/main/java/com/example/bookstore/controller/BookController.java
+++ src/main/java/com/example/bookstore/controller/BookController.java
@@ -1,13 +1,16 @@
 package com.example.bookstore.controller;
 
 import com.example.bookstore.dto.BookDto;
+import com.example.bookstore.dto.BookSearchParametersDto;
 import com.example.bookstore.model.Book;
 import com.example.bookstore.service.BookService;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,6 +28,7 @@ public class BookController {
 
     @GetMapping("/{id}")
     public BookDto getBookById(@PathVariable Long id) {
+
         return bookService.getBookById(id);
     }
 
@@ -32,4 +36,21 @@ public class BookController {
     public Book save(@RequestBody Book book) {
         return bookService.save(book);
     }
+
+    @GetMapping("/search")
+    public List<BookDto> searchBooks(BookSearchParametersDto searchParameters) {
+        return bookService.search(searchParameters);
+    }
+
+    @PutMapping("/{id}")
+    public BookDto updateBookById(@RequestBody Book book, @PathVariable Long id) {
+
+        return bookService.update(book, id);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public void deleteBookById(@PathVariable Long id) {
+        bookService.deleteById(id);
+    }
+
 }
---
+++ src/main/java/com/example/bookstore/repository/BookSpecificationBuilder.java
@@ -0,0 +1,31 @@
+package com.example.bookstore.repository;
+
+import com.example.bookstore.dto.BookSearchParametersDto;
+import com.example.bookstore.model.Book;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.stereotype.Component;
+
+@RequiredArgsConstructor
+@Component
+public class BookSpecificationBuilder implements SpecificationBuilder<Book> {
+    private SpecificationProviderManager<Book> specificationProviderManager;
+
+    @Override
+    public Specification<Book> build(BookSearchParametersDto searchParametersDto) {
+        Specification<Book> specification = Specification.where(null);
+        if (searchParametersDto.getAuthors() != null
+                && searchParametersDto.getAuthors().length > 0) {
+            specification = specification.and(specificationProviderManager
+                    .getSpecificationProvider("author")
+                    .getSpecification(searchParametersDto.getAuthors()));
+        }
+        if (searchParametersDto.getTitles() != null
+                && searchParametersDto.getTitles().length > 0) {
+            specification = specification.and(specificationProviderManager
+                    .getSpecificationProvider("title")
+                    .getSpecification(searchParametersDto.getTitles()));
+        }
+        return specification;
+    }
+}
