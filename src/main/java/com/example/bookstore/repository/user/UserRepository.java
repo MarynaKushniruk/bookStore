ba550cbd393f9f2262c37103c32a9a4878242a64
marinakusniruk <Kushnirukmv@gmail.com>
2023-10-01 11:44:39 +0200

    Practice bookstore changes

--- src/main/java/com/example/bookstore/repository/user/UserRepository.java
+++ src/main/java/com/example/bookstore/repository/user/UserRepository.java
@@ -3,9 +3,7 @@ package com.example.bookstore.repository.user;
 import com.example.bookstore.model.User;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
 }
