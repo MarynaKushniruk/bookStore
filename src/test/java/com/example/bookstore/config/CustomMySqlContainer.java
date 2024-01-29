33af42cb96b8f7eafcfa9e15c69f86368cff130a
marinakusniruk <Kushnirukmv@gmail.com>
2024-01-29 13:46:13 +0100

    tests completed2

--- src/test/java/com/example/bookstore/config/CustomMySqlContainer.java
+++ src/test/java/com/example/bookstore/config/CustomMySqlContainer.java
@@ -1,6 +1,10 @@
 package com.example.bookstore.config;
 
+import static java.time.temporal.ChronoUnit.SECONDS;
+
+import java.time.Duration;
 import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.containers.wait.strategy.Wait;
 
 public class CustomMySqlContainer extends MySQLContainer<CustomMySqlContainer> {
     private static final String DB_IMAGE = "mysql:8";
@@ -23,6 +27,8 @@ public class CustomMySqlContainer extends MySQLContainer<CustomMySqlContainer> {
         System.setProperty("TEST_DB_URL", mysqlContainer.getJdbcUrl());
         System.setProperty("TEST_DB_USERNAME", mysqlContainer.getUsername());
         System.setProperty("TEST_DB_PASSWORD", mysqlContainer.getPassword());
+        mysqlContainer.setWaitStrategy(Wait.defaultWaitStrategy()
+                .withStartupTimeout(Duration.of(60, SECONDS)));
     }
 
     @Override
